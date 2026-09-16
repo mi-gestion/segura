9620bac87ba58962aeb0a4fe0f9b32b09b596ab9
Francisco Suarez <suarezfco65@gmail.com>
2026-09-16 18:11:34 -0400

    Deploy Producción v1.0.12 — Mejora de Experiencia Registro, login y cambio de contraseña: 2026-09-16 18:11:34

---
+++ assets/editor-media.4y98F8R-.js
@@ -0,0 +1,1 @@
+import{B as e}from"./BaseElement.9fGzvbuo.js";import"./Elements.B8W_hxTW.js";import"./FormulaEngine.CYGke5JL.js";class t extends e{render(){}}customElements.define("editor-media",t);export{t as EditorMedia};
